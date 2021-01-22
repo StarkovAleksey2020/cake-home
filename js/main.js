e1ee367e08e3bdcd52d6e31c7793b62adb1a1a02
StarkovAleksey2020 <starkovaleksey2017@gmail.com>
2021-01-22 13:44:56 +0800

    fixed: layout

--- js/main.js
+++ js/main.js
@@ -131,4 +131,72 @@ $(document).ready(function () {
     goTopButton.toggleClass("hiden");
   })
 
+  // кнопка возврата в начало страницы
+  var btn = $('#button');
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 200) {
+      btn.addClass('show');
+    } else {
+      btn.removeClass('show');
+    }
+  });
+  btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+  });
+
+  //валидация email
+  jQuery.validator.addMethod("emailfull", function(value, element) {
+    return this.optional(element) || /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i.test(value);
+  }, "Please enter valid email address!");
+
+  // Обработка форм
+  $('.form').each(function () {
+    $(this).validate({
+      rules: {
+        email: {
+          required: true,
+          email: true,
+          emailfull: true
+        },
+        phone: {
+          required: true,
+          minlength:18
+        }
+      },
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Пожалуйста укажите ваше имя",
+          minlength: "Минимальная длина поля 2 символа",
+        },
+        phone: {
+          required: "Пожалуйста укажите ваш номер телефона",
+          minlength: "Минимальная длина поля 10 символов",
+        },
+        email: {
+          required: "Пожалуйста укажите ваш email",
+          email: "Формат email - name@domain.com",
+        },
+      },
+    });
+  });
+
+  // обработка нажатия ESC
+  $(document).keyup(function (e) {
+    if (e.keyCode === 27) {
+      closeModal(e);
+    }
+  });
+  function closeModal(event) {
+    event.preventDefault();
+    $('input').val('');
+    $('textarea').val('');
+    $("input").removeClass("invalid");
+    $("label").remove(".invalid");
+  }
+
+  // Маска номера телефона
+  $(".phoneInput").mask("+7 (999) 999-99-99");
+
 });
